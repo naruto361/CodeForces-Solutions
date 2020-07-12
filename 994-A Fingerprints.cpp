281fff830a89e4693cd8d2bff4cda46e2b906a18
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-12 22:50:43 +0530

    Implementation

---
+++ 994-A Fingerprints.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() 
+{   
+    int n,m;
+    cin>>n>>m;
+    int a[n],b[m];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    for(int i=0;i<m;i++)
+    cin>>b[i];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i]==b[j]) cout<<a[i]<<" ";
+        }
+    }
+}
