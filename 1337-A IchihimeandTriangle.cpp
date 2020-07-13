e3e4241bde15abe6cff3b6d9f1aaba4fb2af642d
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-13 21:28:21 +0530

    Constructive Algorithm

---
+++ 1337-A IchihimeandTriangle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using  namespace std;
+#define ll long long int
+int main()
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
