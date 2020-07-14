22f9b7d06bebfea52c0bf9436447b0142a3f45e3
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-14 11:54:49 +0530

    Constructive Algorithm

---
+++ 246-A BuggySorting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n<=2)
+    cout<<-1;
+    else
+    {
+        for(int i=n;i>0;i--)
+            cout<<i<<" ";
+    }
+}
