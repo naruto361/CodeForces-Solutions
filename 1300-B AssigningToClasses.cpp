50fcc3398f03f276092ea7401a29d9f3880d74d7
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-15 22:42:37 +0530

    Greedy

---
+++ 1300-B AssigningToClasses.cpp
@@ -0,0 +1,54 @@
+/* .............
+   S A K S H A M
+   .............
+*/   
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout<<"yes\n";
+#define no cout<<"no\n";
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define nl cout<<"\n";
+#define ff first
+#define ss second
+#define loop1(m) for(int i=0;i<m;i++) 
+#define vl vector<ll> 
+#define vll vector<ll,ll>
+#define vc vector<char>
+#define msi map<string,int>
+#define loop2(n,m) for(auto i=n;i<=m;i++)
+#define count1s(n) __builtin_popcount(n)
+bool psqrt(ll x) 
+{   
+  long double sr = sqrt(x); 
+  return ((sr - floor(sr)) == 0); 
+}
+bool ifodd(ll n)
+{
+    return (n&1);
+}
+int lcm(int a, int b)
+{
+    return a / __gcd(a, b) * b;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll s=2*n;
+        ll a[s];
+        loop1(s) cin>>a[i];
+        sort(a,a+s);
+        cout<<a[n]-a[n-1];
+        nl
+    }
+}
