cd0c3159ce2f9b1ada4cdf882c890829c4f3c74b
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-15 14:42:08 +0530

    Implementation

---
+++ 705-A Hulk.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X1 cout<<"I hate";
+#define X2 cout<<"I love";
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(i&1)
+        X1
+        else
+        X2
+        if(i!=n) cout<<" that ";
+    }
+    cout<<" it";
+}
