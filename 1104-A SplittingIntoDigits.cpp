fe9629f9d126cdf46b4d7ecf646b17500584f8ee
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-15 14:10:02 +0530

    Implementation

---
+++ 1104-A SplittingIntoDigits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define ll long long int
+int main()
+{
+    int t;
+    cin>>t;
+    cout<<t<<endl;
+    while(t--)
+    {
+        cout<<1<<" ";
+    }
+    cout<<endl;
+    
+}
