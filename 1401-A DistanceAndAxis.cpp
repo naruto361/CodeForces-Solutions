314a76471c13c2c963f4a9267f1b3764df0b4ea8
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-31 13:37:02 +0530

    Update 1401-A DistanceAndAxis.cpp

--- 1401-A DistanceAndAxis.cpp
+++ 1401-A DistanceAndAxis.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
- 
-int main()  {
+ int main()  
+{
     int t=0;
     cin >> t;
-    while (t--)    {
+    while (t--)    
+    {
         int n=0, k=0;
         cin >> n >> k;
-        if (n > k)  {
-            if (n % 2 == 0 && k % 2 == 0)   {
+        if (n > k)  
+        {
+            if (n % 2 == 0 && k % 2 == 0)   
                 cout << "0" << endl;
-            }   else if (n % 2 == 1 && k % 2 == 1)  {
+            else if (n % 2 == 1 && k % 2 == 1)  
                 cout << "0" << endl;
-            }   else {
+            else 
                 cout << "1" << endl;
-            }
-        }   else {
+        }   
+        else 
             cout << (k-n) << endl;
-        }
     }
     return 0;
 }
