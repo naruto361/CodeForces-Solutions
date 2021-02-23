5d3f07670067a9f42d85890416255174e6d247cf
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-02-23 22:08:46 +0530

    Create 1492-B CardDeck.cpp

---
+++ 1492-B CardDeck.cpp
@@ -0,0 +1,87 @@
+/* .............
+   S A K S H A M
+   .............
+*/   
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout<<"yes\n";
+#define no cout<<"no\n";
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define nl cout<<"\n";
+#define ff first
+#define ss second
+#define loop1(m) for(int i=0;i<m;i++) 
+#define vl vector<ll> 
+#define vll vector<ll,ll>
+#define vc vector<char>
+#define msi map<string,int>
+#define loop2(n,m) for(auto i=n;i<=m;i++)
+#define count1s(n) __builtin_popcount(n)
+bool psqrt(ll x) 
+{   
+  long double sr = sqrt(x); 
+  return ((sr - floor(sr)) == 0); 
+}
+bool ifodd(ll n)
+{
+    return (n&1);
+}
+int lcm(int a, int b)
+{
+    return a / __gcd(a, b) * b;
+}
+bool sortbysec(const pair<int,int> &a, 
+              const pair<int,int> &b) 
+{ 
+    return (a.second < b.second); 
+}
+bool isprime(ll n) 
+{ 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+    if (n%2 == 0 || n%3 == 0) return false; 
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+    return true; 
+} 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++) cin>>a[i];
+        int maxi=a[0];
+        vector<vector<int>> pos;
+        vector<int> v;
+        v.push_back(a[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]>maxi)
+            {
+                maxi=a[i];
+                pos.push_back(v);
+                v.clear();
+                v.push_back(a[i]);
+            }
+            else v.push_back(a[i]);
+        }
+        if(v.size()>0) pos.push_back(v);
+        for(int i=pos.size()-1;i>=0;i--)
+        {
+            for(int j=0;j<pos[i].size();j++)
+            cout<<pos[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
